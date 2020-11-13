6190dbd8ef0d6e66447b1184371b5fc4ddbde318
Jackie <taekwondo.chan@gmail.com>
2020-11-12 17:56:36 -0800

    complete part 1

---
+++ dog.py
@@ -0,0 +1,8 @@
+class Dog:
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+        print("dog intialized")
+
+    def bark(self):
+        print("Woof")
---
+++ my_dogs.py
@@ -0,0 +1,6 @@
+import dog
+
+my_dog = dog.Dog("Rex", "Superdog")
+my_other_dog = dog.Dog("Annie", "Superdog")
+my_dog.bark()
+print(my_other_dog.name)
